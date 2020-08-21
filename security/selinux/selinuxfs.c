a0885bf3f0dbfc78504309f3cb67cc25afbcdde1
Stephen Smalley <stephen.smalley.work@gmail.com>
2020-08-21 16:05:52 -0400

    selinux: dynamically allocate selinux namespace
    Move from static allocation of a single selinux namespace to
    dynamic allocation.  Include necessary support for lifecycle management
    of the selinux namespace, modeled after the user namespace support.
    
    Signed-off-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -90,7 +90,7 @@ static struct selinux_fs_info *selinux_fs_info_create(void)
 		return NULL;
 
 	fsi->last_ino = SEL_INO_NEXT - 1;
-	fsi->state = current_selinux_state;
+	fsi->state = get_selinux_state(current_selinux_state);
 	return fsi;
 }
 
@@ -99,6 +99,7 @@ static void selinux_fs_info_free(struct selinux_fs_info *fsi)
 	unsigned int i;
 
 	if (fsi) {
+		put_selinux_state(fsi->state);
 		for (i = 0; i < fsi->bool_num; i++)
 			kfree(fsi->bool_pending_names[i]);
 		kfree(fsi->bool_pending_names);
